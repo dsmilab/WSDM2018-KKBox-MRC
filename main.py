f380d4a07f5cfaa4c5cbc108e3a8f9507808b808
howeverforever <longbiau@hotmail.com.tw>
2017-11-09 13:45:34 +0800

    [LB 0.65510] Waiting for CV, Gridsearch and pre-process features.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import lightgbm as lgb
 
+import json
 import os.path
 import gc
 
@@ -18,45 +19,11 @@ def transform_isrc_to_year(isrc):
 
 def main():
     print('>> Load CSV data...')
-    train_df = pd.read_csv('data/train.csv', dtype={
-        'msno': 'category',
-        'song_id': 'category',
-        'source_system_tab': 'category',
-        'source_screen_name': 'category',
-        'source_type': 'category',
-        'target': np.int32
-    })
-    test_df = pd.read_csv('data/test.csv', dtype={
-        'msno': 'category',
-        'song_id': 'category',
-        'source_system_tab': 'category',
-        'source_screen_name': 'category',
-        'source_type': 'category',
-        'id': np.int32
-    })
-    songs_df = pd.read_csv('data/songs.csv', dtype={
-        'genre_ids': 'category',
-        'language': 'category',
-        'artist_name': 'category',
-        'composer': 'category',
-        'lyricist': 'category',
-        'song_id': 'category',
-        'song_length': 'category',
-    })
-    song_extra_info_df = pd.read_csv('data/song_extra_info.csv', dtype={
-        'song_id': 'category',
-        'name': 'category',
-        'isrc': 'category'
-    })
-    members_df = pd.read_csv('data/members.csv', dtype={
-        'msno': 'category',
-        'city': 'category',
-        'bd': np.int32,
-        'gender': 'category',
-        'registered_via': 'category',
-        'registration_init_time': 'category',
-        'expiration_date': 'category'
-    })
+    train_df = pd.read_csv('data/train.csv')
+    test_df = pd.read_csv('data/test.csv')
+    songs_df = pd.read_csv('data/songs.csv')
+    song_extra_info_df = pd.read_csv('data/song_extra_info.csv')
+    members_df = pd.read_csv('data/members.csv')
 
     print('>> Merge needed information...')
     song_extra_info_df['song_year'] = song_extra_info_df['isrc'].apply(transform_isrc_to_year)
@@ -76,6 +43,8 @@ def main():
     for column in train_df.columns:
         if train_df[column].dtype == object:
             train_df[column] = train_df[column].astype('category')
+    for column in test_df.columns:
+        if test_df[column].dtype == object:
             test_df[column] = test_df[column].astype('category')
 
     x = train_df.drop(['target'], axis=1)
@@ -95,26 +64,34 @@ def main():
         params = dict({
             'learning_rate': 0.1,
             'application': 'binary',
-            'max_depth': 8,
-            'num_leaves': 2 ** 8,
+            'min_data_in_leaf': 4,
+            'max_depth': 6,
+            'num_leaves': 2 ** 6,
             'verbosity': 0,
             'metric': 'auc'
         })
 
         model = lgb.train(params, train_set=train_set, valid_sets=valid_set, num_boost_round=100)
         model.save_model(MODEL_FILE_NAME)
+        model_json = model.dump_model(model.best_iteration)
+        json.dump(model_json, open('model_result.json', 'w'), indent=4)
+        # feature names
+        print('Feature names:', model.feature_name())
+
+        # feature importance
+        print('Feature importance:', list(model.feature_importance()))
+
         print('>> >> Done!')
 
     print('>> Load model configure...')
     model = lgb.Booster(model_file=MODEL_FILE_NAME)
-
+    print(model.feature_importance())
     print('>> Predicting...')
     y_test = model.predict(x_test)
     submission_df = pd.DataFrame()
     submission_df['id'] = test_ids
     submission_df['target'] = y_test
     submission_df.to_csv('data/submission.csv', index=False)
-    print(submission_df)
 
     print('>> Done!')
 
