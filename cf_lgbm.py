73aea511d930e01512f08e589e116a11571afd1d
ctjoy <ctjoychen@gmail.com>
2017-12-06 14:34:46 +0800

    rewrite cf_lgbm

--- cf_lgbm.py
+++ cf_lgbm.py
@@ -5,73 +5,87 @@ import logging
 import pickle
 import os
 import json
-from utils.data import ImplicitProcesser
+from utils.data import ImplicitProcesser, FeatureProcesser
 import lightgbm as lgb
 
 
-MODEL_FILE_NAME = 'lgbm.txt'
-MODEL_DIR = './model'
-LOG_FORMAT = '%(asctime)s %(levelname)s %(message)s'
+pd.options.mode.chained_assignment = None  # default='warn'
 
-def save(target, name):
-    pickle.dump(target, open(os.path.join(MODEL_DIR, name), 'wb'))
+LOG_FORMAT = '%(asctime)s %(levelname)s %(message)s'
+params = [{
+        'objective': 'binary',
+        'metric': 'binary_logloss',
+        'boosting': 'gbdt',
+        'learning_rate': 0.3 ,
+        'verbose': 0,
+        'num_leaves': 108,
+        'bagging_fraction': 0.95,
+        'bagging_freq': 1,
+        'bagging_seed': 1,
+        'feature_fraction': 0.9,
+        'feature_fraction_seed': 1,
+        'max_bin': 256,
+        'max_depth': 30,
+        'num_rounds': 200,
+        'metric' : 'auc'
+    },{
+        'objective': 'binary',
+        'metric': 'binary_logloss',
+        'boosting': 'dart',
+        'learning_rate': 0.3 ,
+        'verbose': 0,
+        'num_leaves': 108,
+        'bagging_fraction': 0.95,
+        'bagging_freq': 1,
+        'bagging_seed': 1,
+        'feature_fraction': 0.9,
+        'feature_fraction_seed': 1,
+        'max_bin': 256,
+        'max_depth': 20,
+        'num_rounds': 200,
+        'metric' : 'auc'
+    }]
 
 def main():
-    logging.debug('>> Train implicit model ...')
+    logging.debug('>> Get features ...')
 
-    preprocessor = ImplicitProcesser(root='./data',
-                                     feature_size=50,
-                                     real_test=True,
+    feature_processer = FeatureProcesser(root='./data')
+    cf_processer = ImplicitProcesser(feature_size=50,
                                      iterations=30,
                                      calculate_training_loss=True,
                                      save_dir='./model',
-                                     rm_rare=False)
+                                     random_state=50,
+                                     n_clusters=50,
+                                     cluster=True)
 
-    X_train, y_train = preprocessor.load(train=True)
-    X_test, y_id = preprocessor.load(train=False)
+    train, test, unknown_msno_map, unknown_song_map = feature_processer.load()
+    # train.to_csv('train.csv', index=False)
+    # test.to_csv('test.csv', index=False)
 
-    if not os.path.exists(MODEL_FILE_NAME):
-        logging.debug('>> Model configure not found!')
-        logging.debug('>> Create model...')
+    X_train, y_train, X_test, ids = cf_processer.fit(train_df=train, test_df=test, unknown_msno_map=unknown_msno_map, unknown_song_map=unknown_song_map)
 
-        # First, no CV
-        train_set = lgb.Dataset(X_train, y_train)
-        valid_set = [train_set]
+    d_train_final = lgb.Dataset(X_train, y_train)
+    watchlist_final = lgb.Dataset(X_train, y_train)
 
-        params = dict({
-            'learning_rate': 0.1,
-            'application': 'binary',
-            'min_data_in_leaf': 4,
-            'max_depth': 8,
-            'num_leaves': 2 ** 8,
-            'verbosity': 0,
-            'num_iterations': 350,
-            'metric': 'auc'
-        })
+    model_f1 = lgb.train(params[0], train_set=d_train_final,  valid_sets=watchlist_final, verbose_eval=5)
+    model_f2 = lgb.train(params[1], train_set=d_train_final,  valid_sets=watchlist_final, verbose_eval=5)
 
-        model = lgb.train(params, train_set=train_set, valid_sets=valid_set, num_boost_round=100)
-        # model.save_model(os.path.join(MODEL_DIR, MODEL_FILE_NAME))
-        # model_json = model.dump_model(model.best_iteration)
-        # json.dump(model_json, open(os.path.join(MODEL_DIR, 'model_result.json'), 'w'), indent=4)
-        # feature names
-        logging.debug('Feature names:', model.feature_name())
+    print('Making predictions')
+    p_test_1 = model_f1.predict(X_test)
+    p_test_2 = model_f2.predict(X_test)
+    p_test_avg = np.mean([p_test_1, p_test_2], axis = 0)
 
-        # feature importance
-        # logging.debug('Feature importance:', list(model.feature_importance()))
+    print('Done making predictions')
 
-        logging.debug('>> Done!')
+    print ('Saving predictions Model model of gbdt')
 
-    # logging.debug('>> Load model configure...')
-    # model = lgb.Booster(model_file=os.path.join(MODEL_DIR, MODEL_FILE_NAME))
-    # logging.debug(model.feature_importance())
-    logging.debug('>> Predicting...')
-    y_test = model.predict(X_test)
-    result = pd.DataFrame()
-    result['id'] = y_id
-    result['target'] = y_test
-    result.to_csv('./submit/submission.csv', index=False)
+    subm = pd.DataFrame()
+    subm['id'] = ids
+    subm['target'] = p_test_avg
+    # subm['target'] = p_test_1
+    subm.to_csv('./submit/submission_lgbm_avg.csv.gz', compression = 'gzip', index=False, float_format = '%.5f')
 
-    logging.debug('>> Done!')
+    print('Done!')
 
 if __name__ == '__main__':
 
