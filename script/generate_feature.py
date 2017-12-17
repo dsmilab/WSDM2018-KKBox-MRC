774ac86c7b7ff01831e964279b96c1bfe1b5b16c
howeverforever <longbiau@hotmail.com.tw>
2017-12-17 21:30:29 +0800

    Test compatibility

--- script/generate_feature.py
+++ script/generate_feature.py
@@ -5,13 +5,14 @@ import logging
 import pickle
 import os
 import json
-from utils.refactor import FeatureProducer
+from module.FeatureProducer import FeatureProducer
 import lightgbm as lgb
 
 LOG_FORMAT = '%(asctime)s %(levelname)s << %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT, datefmt='%H:%M:%S')
 
 pd.options.mode.chained_assignment = None  # default='warn'
+
 
 def default_value(t):
     if 'object' in str(t):
@@ -21,12 +22,13 @@ def default_value(t):
     else:
         return 0
 
+
 def main():
-    fp = FeatureProducer(root='./data')
+    fp = FeatureProducer(root='../data')
     fp.load_raw()
     fp.pre_process()
-    # fp.feature_engineering()
-    # fp.compute_msno_song_similarity()
+    fp.feature_engineering()
+    # fp.self_fit_transform()
 
     train_dict = {'cols': list(fp.train_df.columns), 'type': [[default_value(t)] for t in fp.train_df.dtypes]}
     test_dict = {'cols': list(fp.test_df.columns), 'type': [[default_value(t)] for t in fp.test_df.dtypes]}
@@ -34,7 +36,9 @@ def main():
     with open('feature/cols.json', 'w') as f:
         json.dump({'train': train_dict, 'test': test_dict}, f)
 
-    fp.train_df.to_csv('feature/re_train.csv', index=False)
-    fp.test_df.to_csv('feature/re_test.csv', index=False)
+    fp.train_df.to_csv('feature/re_train.csv', index=False, header=False)
+    fp.test_df.to_csv('feature/re_test.csv', index=False, header=False)
+
+
 if __name__ == '__main__':
     main()
